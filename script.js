53b16474d720a161386828f2f41a9816d3a5912a
Skyler <skyler.m.degrote@outlook.com>
2015-07-20 21:34:49 -0500

    week 1 complete and week 2 started

--- script.js
+++ script.js
@@ -5,48 +5,48 @@ $(document).ready(function(){
     });
 
     $("#showall").click(function(){
-        $(".html,.general,.javascript,.jquery,.bootstrap,.prototype, .css").slideDown(1000);
+        $(".development,.html,.general,.javascript,.jquery,.bootstrap,.prototype, .css").slideDown(1000);
         $("aside").slideUp();
     });  
 
     $("#css").click(function(){
-        $(".html,.general,.javascript,.jquery,.bootstrap,.prototype").slideUp(1000);
+        $(".development,.html,.general,.javascript,.jquery,.bootstrap,.prototype").slideUp(1000);
         $(".css").slideDown(1000);
         $("aside").slideUp();
     });
 
     $("#html").click(function(){
-        $(".css,.general,.javascript,.jquery,.bootstrap,.prototype").slideUp(1000);
+        $(".development,.css,.general,.javascript,.jquery,.bootstrap,.prototype").slideUp(1000);
         $(".html").slideDown(1000);
         $("aside").slideUp();
     });
 
     $("#general").click(function(){
-        $(".css,.html,.javascript,.jquery,.bootstrap,.prototype").slideUp(1000);
+        $("development,.css,.html,.javascript,.jquery,.bootstrap,.prototype").slideUp(1000);
         $(".general").slideDown(1000);
         $("aside").slideUp();
     });
 
     $("#prototype").click(function(){
-        $(".css,.html,.javascript,.jquery,.bootstrap,.general").slideUp(1000);
+        $("development,.css,.html,.javascript,.jquery,.bootstrap,.general").slideUp(1000);
         $(".prototype").slideDown(1000);
         $("aside").slideUp();
     });
 
     $("#jquery").click(function(){
-        $(".css,.html,.javascript,.prototype,.bootstrap,.general").slideUp(1000);
+        $(".development,.css,.html,.javascript,.prototype,.bootstrap,.general").slideUp(1000);
         $(".jquery").slideDown(1000);
         $("aside").slideUp();
     });
 
     $("#bootstrap").click(function(){
-        $(".css,.html,.javascript,.prototype,.jquery,.general").slideUp(1000);
+        $(".development,.css,.html,.javascript,.prototype,.jquery,.general").slideUp(1000);
         $(".bootstrap").slideDown(1000);
         $("aside").slideUp();
     });
 
     $("#javascript").click(function(){
-        $(".css,.html,.jquery,.prototype,.bootstrap,.general").slideUp(1000);
+        $(".development,.css,.html,.jquery,.prototype,.bootstrap,.general").slideUp(1000);
         $(".javascript").slideDown(1000);
         $("aside").slideUp();
     });
@@ -60,25 +60,37 @@ $(document).ready(function(){
 
 
 
-    $("#ifelsestructure").click(function(){
-        $(".whatjavascript,.whatdevelop,.console,.partsDevelop,.whatprime,.greeting,.prototyping,.htmlcss,.nocss").slideUp(1000);
+   /* $("#ifelsestructure").click(function(){
+        $(".whatjavascript,.functionStructure,.variableDeclare,.dayOne,.dayTwo,.dry,.colors,.whatdevelop,.console,.partsDevelop,.whatprime,.greeting,.prototyping,.htmlcss,.nocss").slideUp(1000);
         $(".ifelsestructure").slideDown(1000);
         $("aside").slideUp();
     });
 
     $("#console").click(function(){
-        $(".ifelsestructure,.whatjavascript,.prototyping,.whatdevelop,.console,.partsDevelop,.whatprime,.greeting,.prototyping,.htmlcss,.nocss").slideUp(1000);
+        $(".ifelsestructure,.functionStructure,.variableDeclare,.dayOne,.dayTwo,.dry,.colors,.whatjavascript,.prototyping,.whatdevelop,.partsDevelop,.whatprime,.greeting,.htmlcss,.nocss").slideUp(1000);
         $(".console").slideDown(1000);
         $("aside").slideUp();
     });
 
     $("#partsDevelop").click(function(){
-        $(".ifelsestructure,.nocss,.console,.whatjavascript,.whatdevelop,.whatprime,.greeting,.htmlcss,.prototyping").slideUp(1000);
+        $(".ifelsestructure,.functionStructure,.variableDeclare,.dayOne,.dayTwo,.dry,.colors,.nocss,.console,.whatjavascript,.whatdevelop,.whatprime,.greeting,.htmlcss,.prototyping").slideUp(1000);
         $(".partsDevelop").slideDown(1000);
         $("aside").slideUp();
-    });
+    });*/
 
-    
+    $("#jquery").click(function(){
+        $(".functionStructure,.partsDevelop,.dry,.colors,.ifelsestructure,.variableDeclare,.dayOne,.dayTwo,.nocss,.console,.whatjavascript,.whatdevelop,.whatprime,.greeting,.htmlcss,.prototyping").slideUp(1000);
+        $(".jquery").slideDown(1000);
+        $("aside").slideUp();
+    }); 
+    $("#objects").click(function(){
+        $(".dry,.partsDevelop,.colors,.functionStructure, .variableDeclare, .dayOne,.dayTwo,.ifelsestructure,.nocss,.console,.whatjavascript,.whatdevelop,.whatprime,.greeting,.htmlcss,.prototyping").slideUp(1000);
+        $(".objects").slideDown(1000);
+        $("aside").slideUp();
+    });
+   
+
+
 
 
 
